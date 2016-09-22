501f67c417cc07f9bb05f1769d9b58c7005f7c0c
Mary <dragonfly_gem@hotmail.com>
2016-09-23 07:07:17 +1000

    Set AWS region

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,6 +83,7 @@ config.action_mailer.default_url_options = { host: "https://thawing-bastion-1687
 # Sets Paperclip to upload images to Amazon S3
 config.paperclip_defaults = {
   :storage => :s3,
+  :s3_region => ENV['AWS_REGION'],
   :s3_credentials => {
     :bucket => ENV['S3_BUCKET_NAME'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
