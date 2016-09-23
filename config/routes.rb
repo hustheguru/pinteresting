48792de999f20809480ae6965e6b822dfc7cc8a8
Mary <dragonfly_gem@hotmail.com>
2016-09-23 19:43:11 +1000

    Update show view, add glyphicons, and change root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
  Rails.application.routes.draw do
   resources :pins
   devise_for :users
-  root to: 'pages#home'
+  root to: 'pins#index'
 
   get 'about' => 'pages#about' #creates above tab
   get 'faqs' => 'pages#faqs'
